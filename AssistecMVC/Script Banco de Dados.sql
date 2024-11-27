5f3483f296ab65400c4ebfed97d2275e00d439f1
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-11-27 09:12:20 -0300

    .

--- AssistecMVC/Script Banco de Dados.sql
+++ AssistecMVC/Script Banco de Dados.sql
@@ -4,14 +4,14 @@ USE `lojabd`;
 
 CREATE TABLE `usuarios` (
   `iduser` int(11) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
+  `nome` varchar(50) NOT NULL,
   `fone` varchar(15) NOT NULL,
   `login` varchar(15) NOT NULL,
   `senha` varchar(15) NOT NULL,
   `perfil` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `usuarios` (`iduser`, `usuario`, `fone`, `login`, `senha`, `perfil`) VALUES
+INSERT INTO `usuarios` (`iduser`, `nome`, `fone`, `login`, `senha`, `perfil`) VALUES
 (1, 'BELTRANO FERREIRA', '1199999-9999', 'beltrano', '1234', 'admin'),
 (2, 'FULANO DA SILVA', '1199999-9999', 'fulano', '1234', 'admin'),
 (3, 'ZEZINHO XAVIER', '1199999-9999', 'zezinho', '1234', 'user');
